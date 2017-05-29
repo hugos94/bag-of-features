c2454d6a6d1ff45d561773f8714f61086bb728b7
Hugo Piauilino <hugos94@gmail.com>
2017-05-29 15:58:37 -0300

    Improvements on code finalized.

--- learn.py
+++ learn.py
@@ -24,48 +24,56 @@ def stack_descriptors(features):
     return descriptors
 
 def get_args():
-    # Get the path of the training set
+    # Set and get script arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--trainingSet", help="Path to Training Set", required="True")
+    parser.add_argument("-t", "--trainingSetPath", help="Path to Training Set", required="True")
     parser.add_argument("-c", "--classifierModelFile", help="Classifier Model File", required="True")
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
     return parser.parse_args()
 
 if __name__ == "__main__":
 
+    # Get arguments
     args = get_args()
 
+    # If verbose argument is provided, allow logs to be printed
     if args.verbose:
         log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
     else:
         log.basicConfig(format="%(levelname)s: %(message)s")
 
-    log.info("Algorithm execution time count started")
+    # Start counting execution time
+    log.info("Starting counting execution time")
     start_time = time.time()
 
     # Get the training classes names and store them in a list
     log.info("Getting the training classes names and store them in a list")
-    train_path = args.trainingSet
-    training_names = os.listdir(train_path)
+    training_names = os.listdir(args.trainingSetPath)
 
-    # Get all the path to the images and save them in a list
-    # image_paths and the corresponding label in image_paths
+    # Get all the path to the images and save them in a list with image_paths and the corresponding label in image_paths
     log.info("Getting all the path to the images and save them in a list")
     image_paths = []
     image_classes = []
     class_id = 0
     for training_name in training_names:
-        dir = os.path.join(train_path, training_name) # Training directories
-        class_path = imutils.imlist(dir)
+        directory_name = os.path.join(args.trainingSetPath, training_name)
+        class_path = imutils.imlist(directory_name)
         image_paths += class_path
         image_classes += [class_id]*len(class_path)
         class_id += 1
 
+    # Get the amount of cpus
     cpus = os.cpu_count()
-    path_size = len(image_paths)
-    path_lists_size = int(numpy.ceil(len(image_paths)/cpus))
-    log.info("Dividing feature extraction between {} cpus".format(cpus))
-    image_paths_parts = [image_paths[i:i + path_lists_size] for i in range(0, path_size, path_lists_size)]
+
+    # Take the set size
+    set_size = len(image_paths)
+
+    # Calculates the number of subsets required for the quantity of cpus
+    subset_size = int(numpy.ceil(set_size/cpus))
+
+    # Divide the set into subsets according to the quantity of cpus
+    log.info("Dividing feature detection and extraction between {} processes".format(cpus))
+    image_paths_parts = [image_paths[i:i + subset_size] for i in range(0, set_size, subset_size)]
 
     # Create feature extraction and keypoint detector objects
     log.info("Create feature extraction and keypoint detector objects")
@@ -75,7 +83,7 @@ if __name__ == "__main__":
     log.info("Detecting points and extracting features")
     features = pool.map(detectAndCompute, (image_paths_parts))
 
-    # Stack all the descriptors vertically in a numpy array
+    # Stack all the descriptors vertically in a numpy array using Pool
     log.info("Stack all the descriptors vertically in a numpy array")
     descriptors = pool.map(stack_descriptors, (features))
     descriptors_result = descriptors.pop(0)
@@ -89,7 +97,7 @@ if __name__ == "__main__":
 
     # Calculate the histogram of features
     log.info("Calculate the histogram of features")
-    im_features = numpy.zeros((len(image_paths), k), "float32")
+    im_features = numpy.zeros((set_size, k), "float32")
     i = 0
     for feature in features:
         for _ , descriptor in feature:
@@ -113,6 +121,6 @@ if __name__ == "__main__":
     bof = args.classifierModelFile + ".pkl"
     joblib.dump((clf, training_names, stdSlr, k, voc), bof, compress = 3)
 
-    # Print algorithm execution time in seconds
-    log.info("Algorithm execution time count ended")
+    # Stopping counting execution time
+    log.info("Stopping counting execution time")
     log.info("--- %s seconds ---" % (time.time() - start_time))
