f575d5fc60e87832c279403a83f347276c54ed65
Hugo Piauilino <hugos94@gmail.com>
2017-09-21 15:53:21 -0300

    Extra prints removed. Folder creating problem solved. Fast detector added.

--- detect-and-compute-keypoints.py
+++ detect-and-compute-keypoints.py
@@ -39,11 +39,12 @@ if __name__ == "__main__":
         print("No such directory {}. Check if the directory exists".format(args.imageSetPath))
         exit()
 
-    try:
-        os.makedirs(args.featuresPath)
-    except OSError:
-        print("Can't create a folder on " + args.featuresPath)
-        exit()
+    if not os.path.isdir(args.featuresPath):
+        try:
+            os.makedirs(args.featuresPath)
+        except OSError:
+            print("Can't create a folder on " + args.featuresPath)
+            exit()
 
     images_paths = []
     images_classes = []
@@ -66,7 +67,7 @@ if __name__ == "__main__":
     subset_size = int(numpy.ceil(set_size/cpus))
 
     # Divide the set into subsets according to the quantity of cpus
-    print("Dividing feature detection and extraction between {} processes".format(cpus))
+    # print("Dividing feature detection and extraction between {} processes".format(cpus))
     images_paths_parts = [images_paths[i:i + subset_size] for i in range(0, set_size, subset_size)]
 
     # Descriptors declaration
@@ -86,12 +87,20 @@ if __name__ == "__main__":
             keypoint_detector = cv2.xfeatures2d.SURF_create()
             detector_name += "SURF"
             print("SURF")
+        elif detector_number == 2:
+            keypoint_detector = cv2.FastFeatureDetector_create()
+            detector_name += "FAST"
+            print("FAST")
+        # elif detector_number == 3:
+        #     keypoint_detector = cv2.ORB_create()
+        #     detector_name += "ORB"
+        #     print("ORB")
 
+        print("Descriptor")
         descriptor_number = 0
         while True:
             descriptor_name = "+descriptor_"
             #Choosing the descriptor
-            print("Descriptor")
             if descriptor_number == 0:
                 keypoint_descriptor = cv2.xfeatures2d.SIFT_create()
                 descriptor_name += "SIFT"
@@ -102,22 +111,22 @@ if __name__ == "__main__":
                 print("SURF")
 
             # Create feature extraction and keypoint detector objects
-            print("Create feature extraction and keypoint detector objects")
+            # print("Create feature extraction and keypoint detector objects")
             pool = Pool(processes=cpus)
 
             # Detecting points and extracting features
-            print("Detecting points and extracting features")
+            # print("Detecting points and extracting features")
             keypoints_descriptions = pool.map(detect_and_compute_keypoints, (images_paths_parts))
 
             # Stack all the descriptors vertically in a numpy array using Pool
-            print("Stack all descriptors vertically in a numpy array")
+            # print("Stack all descriptors vertically in a numpy array")
             descriptors = pool.map(stack_descriptors, (keypoints_descriptions))
             descriptors_result = descriptors.pop(0)
             for descriptor in descriptors:
                 descriptors_result = numpy.concatenate((descriptors_result, descriptor), axis=0)
 
             # Save the SVM
-            print("Saving descriptions")
+            # print("Saving descriptions")
             features_path = args.featuresPath+detector_name+descriptor_name+".pkl"
             joblib.dump((descriptors_result),features_path, compress = 0)
 
@@ -126,5 +135,5 @@ if __name__ == "__main__":
                 break
 
         detector_number += 1
-        if detector_number == 2:
+        if detector_number == 3:
             break
